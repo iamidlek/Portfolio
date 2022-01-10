4a4ec51fefbbb68739384e26ee1cc948ce5505c0
iamidlek <iamidlek@naver.com>
2022-01-10 16:51:45 +0900

    conf: use styledComponets and images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  experimental: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
+};
