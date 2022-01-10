b8112df70ce844484079bdae0bf9f37a45ff2402
iamidlek <iamidlek@naver.com>
2022-01-10 17:04:34 +0900

    feat: ThemeProvider component

---
+++ src/styles/theme.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "styled-components";
+
+import { themeDefault } from "../themes/default";
+import { GlobalStyles } from "./globals";
+
+const Theme = ({ children }: any) => (
+  <ThemeProvider theme={themeDefault}>
+    <GlobalStyles {...themeDefault} />
+    {children}
+  </ThemeProvider>
+);
+
+export default Theme;
