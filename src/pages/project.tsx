20689992f761f2399dbcff1c6d724520094a443e
iamidlek <iamidlek@naver.com>
2022-01-11 22:34:53 +0900

    feat: create project page

---
+++ src/pages/project.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ContentPageContainer } from "../components/Layout/HomePageStyle";
+
+const project = () => {
+  return <ContentPageContainer maxWidth={1024}></ContentPageContainer>;
+};
+
+export default project;
