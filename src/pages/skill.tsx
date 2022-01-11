8cbdee628f3b267345faf943a111d5859e68c97e
iamidlek <iamidlek@naver.com>
2022-01-12 02:25:42 +0900

    feat: skill page act complete

--- src/pages/skill.tsx
+++ src/pages/skill.tsx
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import { skilldata, skillCategory } from "../../public/skilldata";
 import RadialBar from "../components/Chart/RadialBar";
 import { Grid } from "../components/Layout/Grid";
 import { ContentPageContainer } from "../components/Layout/HomePageStyle";
+import { AnimatePresence } from "framer-motion";
+import { Text, TitleBox } from "../components/Home/ContextStyle";
+import { Button } from "../components/Layout/Btn";
+import { BsFillCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
 
-const skill = () => {
+const variants = {
+  invisible: {
+    x: 500,
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+  exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 1 } },
+};
+
+const Skill = () => {
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 5 ? 5 : prev + 1));
+  const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
   return (
     <ContentPageContainer maxWidth={1024}>
-      <Grid>
-        <RadialBar />
-        <div style={{ border: "1px solid red" }}>ssddd</div>
-        <div style={{ border: "1px solid red" }}>ssddd</div>
-        <div style={{ border: "1px solid red" }}>ssddd</div>
-        <div style={{ border: "1px solid red" }}>ssddd</div>
-        <div style={{ border: "1px solid red" }}>ssddd</div>
-        <div style={{ border: "1px solid red" }}>ssddd</div>
-        <div style={{ border: "1px solid red" }}>ssddd</div>
-        <div style={{ border: "1px solid red" }}>ssddd</div>
-        <div style={{ border: "1px solid red" }}>ssddd</div>
-        <div style={{ border: "1px solid red" }}>ssddd</div>
-      </Grid>
+      <TitleBox>
+        <Text>{skillCategory[visible]}</Text>
+      </TitleBox>
+      <AnimatePresence>
+        {[1, 2, 3, 4, 5].map((tab) =>
+          tab === visible ? (
+            <Grid
+              variants={variants}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+              key={tab}
+            >
+              {skilldata[tab].map((skill) => (
+                <RadialBar key={skill.name} {...skill} />
+              ))}
+            </Grid>
+          ) : null
+        )}
+      </AnimatePresence>
+      <Button onClick={prevPlease} disabled={1 === visible}>
+        <BsFillCaretLeftFill style={{ color: "rgba(81,90,218,1)" }} />
+        prev
+      </Button>
+      <Button onClick={nextPlease} right disabled={5 === visible}>
+        next
+        <BsFillCaretRightFill style={{ color: "rgba(81,90,218,1)" }} />
+      </Button>
     </ContentPageContainer>
   );
 };
 
-export default skill;
+export default Skill;
