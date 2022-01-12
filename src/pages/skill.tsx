609885cf9aa9f7b6c9bf4397da770aab04cce791
iamidlek <iamidlek@naver.com>
2022-01-12 13:12:15 +0900

    feat: infinite slide

--- src/pages/skill.tsx
+++ src/pages/skill.tsx
@@ -1,34 +1,17 @@
 import React, { useState } from "react";
 import { skilldata, skillCategory } from "../../public/skilldata";
 import RadialBar from "../components/Chart/RadialBar";
-import { Grid } from "../components/Layout/Grid";
+import { Grid, variants } from "../components/Layout/Grid";
 import { ContentPageContainer } from "../components/Layout/HomePageStyle";
 import { AnimatePresence } from "framer-motion";
 import { Text, TitleBox } from "../components/Home/ContextStyle";
 import { Button } from "../components/Layout/Btn";
 import { BsFillCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
 
-const variants = {
-  invisible: {
-    x: 500,
-    opacity: 0,
-    scale: 0,
-  },
-  visible: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 1,
-    },
-  },
-  exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 1 } },
-};
-
 const Skill = () => {
   const [visible, setVisible] = useState(1);
-  const nextPlease = () => setVisible((prev) => (prev === 5 ? 5 : prev + 1));
-  const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  const nextPlease = () => setVisible((prev) => (prev === 5 ? 1 : prev + 1));
+  const prevPlease = () => setVisible((prev) => (prev === 1 ? 5 : prev - 1));
   return (
     <ContentPageContainer maxWidth={1024}>
       <TitleBox>
@@ -51,11 +34,11 @@ const Skill = () => {
           ) : null
         )}
       </AnimatePresence>
-      <Button onClick={prevPlease} disabled={1 === visible}>
+      <Button onClick={prevPlease}>
         <BsFillCaretLeftFill style={{ color: "rgba(81,90,218,1)" }} />
         prev
       </Button>
-      <Button onClick={nextPlease} right disabled={5 === visible}>
+      <Button onClick={nextPlease} right>
         next
         <BsFillCaretRightFill style={{ color: "rgba(81,90,218,1)" }} />
       </Button>
