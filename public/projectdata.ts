aa6b722e58d6d4e2b2344fd6cf9cc946aca8926f
iamidlek <iamidlek@naver.com>
2022-01-13 15:38:45 +0900

    assets

---
+++ public/projectdata.ts
@@ -0,0 +1,72 @@
+export const projects = [
+  {
+    url: "/portfolio_main.png",
+    date: "2022.01",
+    title: "Portfolio",
+    badges: ["Nextjs", "Typescript"],
+    detail:
+      "nextjs를 이용한 정적페이지 생성(SSG)과 <br /> 애니메이션, 차트, pdf 라이브러리를 활용한 포트폴리오 페이지<br />반응형으로 제작된 포트폴리오 페이지",
+    github: "https://github.com/iamidlek/Portfolio",
+    link: undefined,
+  },
+  {
+    url: "/React Mini.png",
+    date: "2021.12",
+    title: "React 쇼핑(장바구니 및 결제)",
+    badges: ["Reactjs", "recoil", "express", "OpenApi"],
+    detail:
+      "로그인 여부에 따른 페이지 접근 권한 설정 기능과<br /> MySQL로 만들어진 테이블과 api를 보고<br /> Material-UI를 사용하여 화면 구현",
+    github: "https://github.com/iamidlek/ExrKDT-ReactMini",
+    link: undefined,
+  },
+  {
+    url: "/ExrKDT-VtoR-notion.png",
+    date: "2021.11",
+    title: "React Notion워크스페이스",
+    badges: ["Reactjs", "redux", "thunk"],
+    detail:
+      "Notion워크스페이스 기능을 구현한 프로젝트<br /> 사진과 글을 작성할 수 있다<br /> 컴포넌트의 재귀적인 사용으로 구현",
+    github: "https://github.com/iamidlek/ExrKDT-VtoR-notion",
+    link: "https://iamidlek.github.io/ExrKDT-VtoR-notion/",
+  },
+  {
+    url: "/Team_mini.png",
+    date: "2021.11",
+    title: "명품 신발 쇼핑몰",
+    badges: ["Vue", "vuex"],
+    detail:
+      "총 5명의 프론트엔드 개발자와 협업하여 구현<br /> 로그인 기능과 admin페이지에서<br />데이터 관리 기능 구현<br />제공받은 api를 이용해 계좌 연결 및 결제 화면 구현",
+    github: "https://github.com/iamidlek/Team_mini",
+    link: "https://sinbalshop.netlify.app",
+  },
+  {
+    url: "/javascript-movie.png",
+    date: "2021.10",
+    title: "영과 검색 사이트",
+    badges: ["javascript", "Intersection Observer"],
+    detail:
+      "바닐라 자바스크립트로 구현한 사이트<br />Intersection Observer를 이용하여<br />스크롤에 따라 보여줄 요소를 선택<br /> 스크롤에 의한 lazy loading 구현",
+    github: "https://github.com/iamidlek/ExrKDT-js-mission",
+    link: "https://kdt-moovle.netlify.app",
+  },
+  {
+    url: "/bankingApp_TP.png",
+    date: "2021.10",
+    title: "뱅킹앱 화면 만들기",
+    badges: ["html", "css", "javascript"],
+    detail:
+      "html, css를 중심으로 개발한 화면<br />javascript로 dragable한 기능을 구현<br />chart.js를 이용한 원형 차트 구현",
+    github: "https://github.com/iamidlek/bankingApp_TP",
+    link: "https://nekobankapp.netlify.app",
+  },
+  {
+    url: "/soylent.png",
+    date: "2021.8",
+    title: "soylent 사이트 클론",
+    badges: ["html", "css", "javascript"],
+    detail:
+      "바닐라 자바스크립트로 슬라이드 기능을 구현한 사이트<br />화면의 다양한 변화를 dom을 컨트롤 하여<br />구현하는 연습을 위한 프로젝트",
+    github: "https://github.com/iamidlek/soylent-clone",
+    link: undefined,
+  },
+];
---
+++ public/skilldata.ts
@@ -0,0 +1,92 @@
+export const skillCategory = [
+  "",
+  "Languages",
+  "Framework",
+  "tools & library",
+  "tools & library",
+  "tools & library",
+];
+
+export const skilldata = [
+  [],
+  [
+    {
+      name: "Javascript",
+      url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+      status: 80,
+    },
+    {
+      name: "typescript",
+      url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+      status: 70,
+    },
+  ],
+  [
+    {
+      name: "react",
+      url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+      status: 75,
+    },
+    {
+      name: "nextjs",
+      url: "https://cdn.worldvectorlogo.com/logos/nextjs-2.svg",
+      status: 70,
+    },
+    {
+      name: "vuejs",
+      url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg",
+      status: 75,
+    },
+  ],
+  [
+    {
+      name: "html5",
+      url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+      status: 90,
+    },
+    {
+      name: "css3",
+      url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+      status: 90,
+    },
+    {
+      name: "sass",
+      url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
+      status: 85,
+    },
+  ],
+  [
+    {
+      name: "babel",
+      url: "https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg",
+      status: 75,
+    },
+    {
+      name: "webpack",
+      url: "https://profilinator.rishav.dev/skills-assets/webpack-original.svg",
+      status: 75,
+    },
+    {
+      name: "redux",
+      url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+      status: 70,
+    },
+  ],
+  [
+    {
+      name: "recoil",
+      url: "https://pbs.twimg.com/profile_images/1377116487933030410/kyyHFjc2_400x400.jpg",
+      status: 75,
+    },
+    {
+      name: "react-query",
+      url: "https://media.vlpt.us/images/woohm402/post/f50d9e7b-910a-4db8-963b-ba19fff6bc78/emblem-light-628080660fddb35787ff6c77e97ca43e.svg",
+      status: 70,
+    },
+    {
+      name: "jest",
+      url: "https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg",
+      status: 55,
+    },
+  ],
+];
